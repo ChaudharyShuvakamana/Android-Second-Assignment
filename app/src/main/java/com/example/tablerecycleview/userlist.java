b8adb82f1dff355ca8d9deebbc6514bc22f6f0eb
Chaudhary Shuvakamana <shuvakamanacdy@gmail.com>
2019-11-14 15:19:43 +0545

    users data can be viewed

---
+++ app/src/main/java/com/example/tablerecycleview/userlist.java
@@ -0,0 +1,34 @@
+package com.example.tablerecycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.tablerecycleview.Module.User;
+
+import java.util.List;
+
+public class userlist extends AppCompatActivity {
+
+    RecyclerView userlistview;
+    List<User> users;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userlist);
+        userlistview = findViewById(R.id.userlistview);
+        Intent i = getIntent();
+        users = (List<User>) i.getSerializableExtra("allusers");
+        userAdapter adapter = new userAdapter(this,users);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        userlistview.setLayoutManager(layoutManager);
+        userlistview.setAdapter(adapter);
+
+
+    }
+}
