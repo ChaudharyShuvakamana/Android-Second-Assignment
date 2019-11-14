fb2e597a254256dff1556d8728f7760ade2c8fc6
Chaudhary Shuvakamana <shuvakamanacdy@gmail.com>
2019-11-14 15:23:51 +0545

    adding name for image

--- app/src/main/java/com/example/tablerecycleview/MainActivity.java
+++ app/src/main/java/com/example/tablerecycleview/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     final List<User> user = new ArrayList<>();
     Spinner country;
     AutoCompleteTextView image;
-    String[] images={"shuvakamana","manoj","milan","","ravi","sadish","sishir","upendra"};
+    String[] images={"shuvakamana","manoj","milan","","ravi","sadish","sishir","upendra","suman"};
     EditText name, dob, phone, email;
     RadioGroup gender;
     Button submit,click;
